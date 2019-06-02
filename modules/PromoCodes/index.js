c3ee814ada0e5eb520b1f941c87e7e417b50b140
Tanmay Vij <tanmayvij047695@gmail.com>
2019-06-02 23:14:54 +0530

    Fix module paths & new routes

--- modules/PromoCodes/index.js
+++ modules/PromoCodes/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.route('/createPromoCode')
+.post(require('./createPromoCode'));
+
+router.route('/usePromoCode')
+.post(require('./usePromoCode'));
+
 module.exports = router;
