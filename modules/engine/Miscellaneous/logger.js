b3740e233e53719e8e59a459f226dfbdc5cd832e
Aaditya Chakravarty <ac36500@gmail.com>
2019-06-21 20:20:08 +0530

    CLR RM

--- modules/engine/Miscellaneous/logger.js
+++ modules/engine/Miscellaneous/logger.js
@@ -1,11 +1,9 @@
-const colors = require('colors');
-
 const loggerFunc = (req, res, next) => {
-    console.log('----------------------------------------'.green);
+    console.log('----------------------------------------');
     console.log('Queries:', req.query);
     console.log('Headers:', req.headers);
     console.log('Body:', req.body);
-    console.log('----------------------------------------'.red);
+    console.log('----------------------------------------');
     next();
 }
 
